2036a8e6eceed3c00fface66c65db87a33f0f186
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-02-10 13:11:19 -0300

    refactor: user signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -19,11 +19,11 @@ router.post("/signup", async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    const user = await User.create({
+    await User.create({
       login,
       password: hashedPassword,
     });
-    res.status(201).json(user);
+    res.status(201).json({ message: "Account created" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
