93773ab71ec47f18cf4b874885af2e5ce7920a9b
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-02-14 11:45:28 -0300

    feat: socket server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const server = express();
+const http = require("http");
+const socketServer = http.createServer(server);
 require("dotenv").config();
 
-server.use(
-  cors()
-);
+server.use(cors());
+
+const io = require("socket.io")(socketServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 const auth = require("./routes/auth");
 server.use("/auth", auth);
@@ -13,6 +20,30 @@ server.use("/auth", auth);
 const user = require("./routes/user");
 server.use("/user", user);
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${process.env.PORT}`);
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
   });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(process.env.DB_PORT, () => {
+  console.log(`DB Server is running on port: ${process.env.DB_PORT}`);
+});
+
+socketServer.listen(process.env.SOCKET_PORT, () => {
+  console.log(`Socket Server is running on port: ${process.env.SOCKET_PORT}`);
+});
