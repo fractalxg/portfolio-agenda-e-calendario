c2b4572a23d223f931ed60b17ebf9bed155abd50
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-02-19 21:34:40 -0300

    feat: loader

--- frontend/src/routes/Signup.jsx
+++ frontend/src/routes/Signup.jsx
@@ -3,26 +3,32 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import BubbleGradient from "../components/BubbleGradient";
+import Loader from "../components/Loader";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState("");
 
   const userSignup = async () => {
     if (validateUsername() && comparePassword()) {
       try {
+        setLoading("loading");
         const response = await axios.post(import.meta.env.VITE_SIGNUP, {
           login: username,
           password: password,
         });
         setMessage(response.data.message);
+        setLoading("loaded");
       } catch (error) {
         console.log(error);
+        setLoading("loaded");
       }
     } else;
     console.log(false);
+    setLoading("loaded");
   };
 
   const validateUsername = () => {
@@ -75,7 +81,12 @@ const Signup = () => {
               onChange={(e) => setConfirmPassword(e.target.value)}
             ></input>
           </div>
-          <div className="signup-content-message">{message}</div>
+          <div className="signup-content-message">
+              {
+              loading === "loading" ? <Loader /> : 
+              loading === "loaded" ? <p>{message}</p> : <p></p>
+              }
+          </div>
 
           <button onClick={() => userSignup()}>Signup</button>
 
